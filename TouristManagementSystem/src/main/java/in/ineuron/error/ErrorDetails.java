706cbf5863cbc651c9e6c466ae4b91614647fe51
sai <sai@DESKTOP-OC4RQ9V>
2023-07-03 20:09:26 +0530

    first commit

---
+++ TouristManagementSystem/src/main/java/in/ineuron/error/ErrorDetails.java
@@ -0,0 +1,17 @@
+package in.ineuron.error;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+	private LocalDateTime ldt;
+	private String msg;
+	private String status;
+}
